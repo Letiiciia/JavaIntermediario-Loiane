e58c7b2d392c2a824ba9a66cb388b34ce0895f56
Letícia Lima <leticialimaferreira2@gmail.com>
2022-02-18 00:50:57 -0300

    Exercicios java intermediario

---
+++ src/minhathread/MinhaThread.java
@@ -0,0 +1,24 @@
+package minhathread;
+
+public class MinhaThread extends Thread{
+	private String nome;
+	private int tempo;
+	
+	
+	public MinhaThread(String nome, int tempo) {
+		this.nome = nome;
+		this.tempo = tempo;
+		start();
+	}
+	
+	public void run() {
+		try {
+			for(int i= 0; i < 6; i++) {
+				System.out.println(nome + " contador " + i);
+				Thread.sleep(100);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
